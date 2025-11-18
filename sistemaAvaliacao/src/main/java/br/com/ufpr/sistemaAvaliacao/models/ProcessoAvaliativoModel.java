f35cf8373aa0deaa284f1737af4a1f39e940210a
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2025-11-18 18:04:51 -0300

    * feat: added ativo and initial DAO to processoAvaliativo

--- sistemaAvaliacao/src/main/java/br/com/ufpr/sistemaAvaliacao/models/ProcessoAvaliativoModel.java
+++ sistemaAvaliacao/src/main/java/br/com/ufpr/sistemaAvaliacao/models/ProcessoAvaliativoModel.java
@@ -22,6 +22,8 @@ public class ProcessoAvaliativoModel {
 
     private String nome;
 
+    private Boolean ativo;
+
     @OneToMany(mappedBy = "processoAvaliativo", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<FormularioModel> formularios;
 }
