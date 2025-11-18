24b73c74fc0c3a92d2f7023bc438d2db5f4b545e
Matheus José <matheusjosclima@outlook.com>
2025-11-18 15:26:43 -0300

    feat: setup inicial do projeto

---
+++ sistemaAvaliacao/src/main/java/br/com/ufpr/sistemaAvaliacao/SistemaDeAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufpr.sistemaAvaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAvaliacaoApplication.class, args);
+	}
+
+}
